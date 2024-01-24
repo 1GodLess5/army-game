afd03499177636d8337f5fa84793aaa19a4c9d5d
1GodLess5 <gacekalbert@seznam.cz>
2024-01-24 21:16:51 +0100

    modified constructor

--- src/main/java/cz/godless/army/Army.java
+++ src/main/java/cz/godless/army/Army.java
@@ -9,10 +9,10 @@ public class Army {
     private List<Spearman> spearmen;
 
     public Army(List<Archer> archers, List<Infantry> infantry, List<Cavalry> cavalry, List<Spearman> spearmen) {
-        this.archers = archers;
-        this.infantry = infantry;
-        this.cavalry = cavalry;
-        this.spearmen = spearmen;
+        this.archers = archers != null ? archers : List.of();
+        this.infantry = infantry != null ? infantry : List.of();;
+        this.cavalry = cavalry != null ? cavalry : List.of();;
+        this.spearmen = spearmen != null ? spearmen : List.of();;
     }
 
 }
