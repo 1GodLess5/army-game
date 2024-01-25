a8d749e108cc16b10758f24ac85f61f5c848a2ca
1GodLess5 <gacekalbert@seznam.cz>
2024-01-25 19:55:29 +0100

    testing classes

--- src/main/java/cz/godless/Main.java
+++ src/main/java/cz/godless/Main.java
@@ -1,7 +1,22 @@
 package cz.godless;
 
+import cz.godless.army.Army;
+import cz.godless.army.Soldier;
+import cz.godless.game.StartGame;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
+        List<String> playersNames = StartGame.greetPlayers();
+        Army userOneArmy = new Army(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        userOneArmy = userOneArmy.createArmy(playersNames.get(0), scanner);
+
+        userOneArmy.reportArmy();
 
     }
 }
