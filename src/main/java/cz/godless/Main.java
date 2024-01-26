e29dbff8c42961504140800e7b3f8bdee31e50ed
1GodLess5 <gacekalbert@seznam.cz>
2024-01-26 07:58:51 +0100

    updated Main

--- src/main/java/cz/godless/Main.java
+++ src/main/java/cz/godless/Main.java
@@ -2,6 +2,7 @@ package cz.godless;
 
 import cz.godless.army.Army;
 import cz.godless.army.Soldier;
+import cz.godless.game.Attack;
 import cz.godless.game.StartGame;
 
 import java.util.ArrayList;
@@ -13,10 +14,14 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
         List<String> playersNames = StartGame.greetPlayers();
-        Army userOneArmy = new Army(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        Army userOneArmy = new Army(new ArrayList<>());
         userOneArmy = userOneArmy.createArmy(playersNames.get(0), scanner);
-
+        Army userTwoArmy = new Army(new ArrayList<>());
+        userTwoArmy = userTwoArmy.createArmy(playersNames.get(1), scanner);
         userOneArmy.reportArmy();
+        userTwoArmy.reportArmy();
+        Attack.attackAgainstDefense(userOneArmy, userTwoArmy, playersNames.get(0), playersNames.get(1));
+        userTwoArmy.reportArmy();
 
     }
 }
