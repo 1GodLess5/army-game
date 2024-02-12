56e9945976a8eebf5b83a1ef521769c27195af43
1GodLess5 <gacekalbert@seznam.cz>
2024-02-12 07:18:16 +0100

    modified attackAgainstDefense(), now calculates users super

--- src/main/java/cz/godless/game/Attack.java
+++ src/main/java/cz/godless/game/Attack.java
@@ -4,28 +4,43 @@ import cz.godless.army.Army;
 import cz.godless.army.Soldier;
 
 public class Attack {
-
-    public static void attackAgainstDefense(Army attackaresArmy, Army deffendersArmy, String attacker, String defender){
+    // TODO continue here, should make special offense method
+    private static int isOnCooldown = 0;
+    public static void attackAgainstDefense(Army attackaresArmy, Army deffendersArmy, String attacker, String defender, boolean isSuper){
         int attackPower = calculatePower(attackaresArmy, "OFFENSIVE");
         int defendPower = calculatePower(deffendersArmy, "DEFENSIVE");
         int randomNumber;
         int totalDamage = 0;
+        int[] defaultStrongerAttackRange = {5, 10};
+        int[] defaultWeakerAttackRange = {1, 5};
+
+        if (isSuper && isOnCooldown == 0 && attackPower > defendPower){
+            defaultStrongerAttackRange[0] += 10;
+            defaultStrongerAttackRange[1] += 20;
+            isOnCooldown = 3;
+        } else if (isSuper && isOnCooldown == 0 && attackPower < defendPower) {
+            defaultWeakerAttackRange[0] += 9;
+            defaultWeakerAttackRange[1] += 15;
+            isOnCooldown = 3;
+        }
+        if (!isSuper && isOnCooldown != 0){
+            isOnCooldown -= 1;
+        }
 
         if (attackPower > defendPower){
             for (Soldier soldier : deffendersArmy.getSoldiers()){
-                randomNumber = getRandom(5, 10);
+                randomNumber = getRandom(defaultStrongerAttackRange[0], defaultStrongerAttackRange[1]);
                 soldier.setHp(randomNumber);
                 totalDamage += randomNumber;
             }
         } else {
             for (Soldier soldier : deffendersArmy.getSoldiers()){
-                randomNumber = getRandom(1, 5);
+                randomNumber = getRandom(defaultWeakerAttackRange[0], defaultWeakerAttackRange[1]);
                 soldier.setHp(randomNumber);
                 totalDamage += randomNumber;
             }
         }
 
-        int index = 0;
         System.out.println("---ATTACK REPORT---");
         System.out.println("Commander's " + attacker + " army attacked " + defender + "army and dealt: ");
         System.out.println("TOTAL DAMAGE OF: " + totalDamage + " hp");
@@ -47,5 +62,9 @@ public class Attack {
 
     private static int getRandom(int min, int max){
         return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    public static int getIsOnCooldown() {
+        return isOnCooldown;
     }
 }
